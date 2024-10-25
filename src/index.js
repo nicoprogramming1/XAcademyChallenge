302dea05ba36132113b9643c29c3badecaddbec2
wnorowsky <wnorowsky.nicolas@gmail.com>
2024-10-25 10:09:36 -0300

    Configure middlewares, settings and server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-const 
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const app = express();
+
+// SETTINGS
+app.set("appName", "ownFIFA");
+app.set("port", 3000);
+app.set("case sensitive routing", true);
+
+// MIDDLEWARES
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:4200",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+// Server static files
+app.use("/static", express.static(path.join(__dirname, "static")));
+
+// SERVER
+app.listen(3000, () => {
+  console.log(`Server ${app.get("appName")} on port: ${3000}`);
+});
