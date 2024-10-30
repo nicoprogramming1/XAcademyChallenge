d0426f044bda7aaff7ede4b0a85d133e64ab537f
wnorowsky <wnorowsky.nicolas@gmail.com>
2024-10-30 11:34:01 -0300

    Add player service,component and route configuration

--- front-end/ownFIFA/src/app/app.routes.ts
+++ front-end/ownFIFA/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'create_player',
+    loadComponent: () =>
+      import(
+        './views/players/player-registration/player-registration.component'
+      ).then((m) => m.PlayerRegistrationComponent),
+  },
+];
---
+++ front-end/ownFIFA/src/app/views/players/player-registration/player-registration.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-player-registration',
+  standalone: true,
+  imports: [],
+  templateUrl: './player-registration.component.html',
+  styleUrl: './player-registration.component.scss'
+})
+export class PlayerRegistrationComponent {
+
+}
