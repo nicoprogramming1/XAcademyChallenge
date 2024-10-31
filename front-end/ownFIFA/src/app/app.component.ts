a30eb3da312c0ab94ae2623c116bcdc125fa80c9
wnorowsky <wnorowsky.nicolas@gmail.com>
2024-10-31 20:48:11 -0300

    Fix angular materials

--- front-end/ownFIFA/src/app/app.component.ts
+++ front-end/ownFIFA/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MatInputModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatDividerModule,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- front-end/ownFIFA/src/app/app.config.ts
+++ front-end/ownFIFA/src/app/app.config.ts
@@ -4,7 +4,8 @@ import { provideRouter, withViewTransitions } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withViewTransitions()), provideClientHydration(), provideHttpClient(withFetch())]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withViewTransitions()), provideClientHydration(), provideHttpClient(withFetch()), provideAnimationsAsync()]
 };
