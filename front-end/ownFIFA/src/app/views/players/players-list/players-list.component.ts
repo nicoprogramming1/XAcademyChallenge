fe6a6089593d881bc9f8c39826498f3d4f305dd6
wnorowsky <wnorowsky.nicolas@gmail.com>
2024-11-01 20:40:46 -0300

    Replace angular materials for bootstrap components

--- front-end/ownFIFA/src/app/views/players/players-list/players-list.component.ts
+++ front-end/ownFIFA/src/app/views/players/players-list/players-list.component.ts
@@ -1,26 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
 import { PlayerService } from '../../../services/player.service';
 import { PlayerStateService } from '../../../services/player-state.service';
-import { Player } from '../../../interfaces/player.interface';
 
 @Component({
   selector: 'app-players-list',
   standalone: true,
   imports: [
     CommonModule,
-    ReactiveFormsModule,
-    MatInputModule,
-    MatSelectModule,
-    MatButtonModule,
-    MatFormFieldModule,
-    MatDividerModule,
+    ReactiveFormsModule
   ],
   templateUrl: './players-list.component.html',
   styleUrl: './players-list.component.scss',
