688e0ae9b07da98b77656a6cc32c058cec402e23
wnorowsky <wnorowsky.nicolas@gmail.com>
2024-11-01 20:26:53 -0300

    Add card-player comp as shared

---
+++ front-end/ownFIFA/src/app/shared/card-player/card-player.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-player',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-player.component.html',
+  styleUrl: './card-player.component.scss'
+})
+export class CardPlayerComponent {
+
+}
--- front-end/ownFIFA/src/app/views/players/players-list/players-list.component.ts
+++ front-end/ownFIFA/src/app/views/players/players-list/players-list.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { PlayerService } from '../../../services/player.service';
+import { PlayerStateService } from '../../../services/player-state.service';
+import { Player } from '../../../interfaces/player.interface';
 
 @Component({
   selector: 'app-players-list',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatDividerModule,
+  ],
   templateUrl: './players-list.component.html',
-  styleUrl: './players-list.component.scss'
+  styleUrl: './players-list.component.scss',
 })
 export class PlayersListComponent {
+  private playerService = inject(PlayerService);
+  private playerStateService = inject(PlayerStateService);
 
+  public loading = this.playerStateService.loading;
+  public error = this.playerStateService.error;
+  public successMessage = this.playerStateService.successMessage;
+  public players = this.playerStateService.players;
+
+  ngOnInit(): void {
+    this.loadPlayers();
+  }
+
+  loadPlayers() {
+    this.playerService.fetchPlayers().subscribe({
+      // actualiza el state de jugadores
+      next: (res) => {
+        console.log(`Se devuelven con éxito ${res?.length} jugadores`);
+      },
+      error: (err) => {
+        console.log('Error al cargar jugadores: ', err.message);
+      },
+    });
+  }
 }
