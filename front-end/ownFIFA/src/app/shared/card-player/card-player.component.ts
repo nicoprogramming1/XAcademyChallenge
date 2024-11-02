443c1168e24c78f1d5c48613d19b767936139d79
wnorowsky <wnorowsky.nicolas@gmail.com>
2024-11-01 21:07:14 -0300

    Add html and logic to card-player component, receives inputs from player-list comp

--- front-end/ownFIFA/src/app/shared/card-player/card-player.component.ts
+++ front-end/ownFIFA/src/app/shared/card-player/card-player.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-player',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './card-player.component.html',
   styleUrl: './card-player.component.scss'
 })
 export class CardPlayerComponent {
-
+  @Input() photoUrl!: string
+  @Input() longName!: string
+  @Input() age!: number
+  @Input() nationality!: string
+  @Input() overall!: number
+  @Input() club!: string
+  @Input() id!: string
 }
