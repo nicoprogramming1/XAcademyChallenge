8351acfe6467627b1c7a605da09646638a454a41
wnorowsky <wnorowsky.nicolas@gmail.com>
2024-11-03 10:47:08 -0300

    Add delay to api calls to test loading spinners

--- front-end/ownFIFA/src/app/services/player.service.ts
+++ front-end/ownFIFA/src/app/services/player.service.ts
@@ -3,11 +3,8 @@ import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { PlayerStateService } from './player-state.service';
 import { Player } from '../interfaces/player.interface';
-import { catchError, finalize, map, Observable, of } from 'rxjs';
-import {
-  PlayerResponse,
-  PlayersResponse,
-} from '../interfaces/responses.interface';
+import { catchError, delay, finalize, map, Observable, of, switchMap } from 'rxjs';
+import { PlayerResponse, PlayersResponse } from '../interfaces/responses.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -18,35 +15,40 @@ export class PlayerService {
   private playerStateService = inject(PlayerStateService);
 
   getAllPlayers(page: number): Observable<PlayersResponse | null> {
-    this.playerStateService.loadingState();
-    return this.http
-      .get<PlayersResponse>(`${this.apiUrl}/player?page=${page}`)
-      .pipe(
-        map((res) => {
-          if (res.success) {
-            this.playerStateService.loadAllPlayersState(res.data.players);
-            console.log(
-              'Desde el service, lista de jugadores: ',
-              res.data.players
-            );
-            return res;
-          } else {
-            throw new Error(res.message);
-          }
-        }),
-        catchError((err) => {
-          this.handleError(err.message);
-          return of(null);
-        }),
-        finalize(() => {
-          this.playerStateService.stopLoadingState();
-        })
-      );
+    this.playerStateService.loadingState(); // define loading: true en el state
+
+    return of(null).pipe(
+      delay(1000), // Retardo de 1 segundo para simular demora y ver los spinner de carga
+      switchMap(() => 
+        this.http.get<PlayersResponse>(`${this.apiUrl}/player?page=${page}`)
+      ),
+      map((res) => {
+        if (res.success) {
+          this.playerStateService.loadAllPlayersState(res.data.players);  // carga los jugadores en el state para ponerlos a disposicion global
+          console.log('Desde el service, lista de jugadores: ', res.data.players);
+          return res;
+        } else {
+          throw new Error(res.message);
+        }
+      }),
+      catchError((err) => {
+        this.handleError(err.message);
+        return of(null);
+      }),
+      finalize(() => {
+        this.playerStateService.stopLoadingState(); // loading: false al state
+      })
+    );
   }
 
   getPlayerById(id: number): Observable<Player | null> {
     this.playerStateService.loadingState();
-    return this.http.get<PlayerResponse>(`${this.apiUrl}/player/${id}`).pipe(
+
+    return of(null).pipe(
+      delay(1000), // Retardo de 1 segundo
+      switchMap(() => 
+        this.http.get<PlayerResponse>(`${this.apiUrl}/player/${id}`)
+      ),
       map((res) => {
         if (res.success) {
           this.playerStateService.loadPlayerState(res.data);
@@ -68,14 +70,16 @@ export class PlayerService {
 
   savePlayer(player: Player): Observable<Player | null> {
     this.playerStateService.loadingState();
-    return this.http.post<PlayerResponse>(`${this.apiUrl}/player`, player).pipe(
+
+    return of(null).pipe(
+      delay(1000), // Retardo de 1 segundo
+      switchMap(() =>
+        this.http.post<PlayerResponse>(`${this.apiUrl}/player`, player)
+      ),
       map((res) => {
         if (res.success) {
           this.playerStateService.savePlayerState(res.data);
-          console.log(
-            'Se ha creado con éxito el/la jugador/a ',
-            res.data.longName
-          );
+          console.log('Se ha creado con éxito el/la jugador/a ', res.data.longName);
           return res.data;
         } else {
           throw new Error(res.message || 'Error desconocido');
@@ -94,7 +98,12 @@ export class PlayerService {
 
   deletePlayer(id: number): Observable<Player | null> {
     this.playerStateService.loadingState();
-    return this.http.delete<PlayerResponse>(`${this.apiUrl}/player/${id}`).pipe(
+
+    return of(null).pipe(
+      delay(1000), // Retardo de 1 segundo
+      switchMap(() => 
+        this.http.delete<PlayerResponse>(`${this.apiUrl}/player/${id}`)
+      ),
       map((res) => {
         if (res.success) {
           this.playerStateService.deletePlayerState(id);
@@ -104,19 +113,18 @@ export class PlayerService {
         }
       }),
       catchError((err) => {
-        const errorMessage =
-          err.message || 'Error en la eliminación del jugador';
+        const errorMessage = err.message || 'Error en la eliminación del jugador';
         this.handleError(errorMessage);
         return of(null);
       }),
       finalize(() => {
-        this.playerStateService.stopLoadingState(); // Asegura que el estado de carga se detenga
+        this.playerStateService.stopLoadingState();
       })
     );
   }
 
   private handleError(err: string) {
-    this.playerStateService.errorState(err);
+    this.playerStateService.errorState(err);  // carga el error en state
     console.error('Se ha producido un error (desde el handleError): ', err);
   }
 }
