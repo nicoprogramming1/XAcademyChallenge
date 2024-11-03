3e8a09e3eff61a659c6606dbfaa31ef456ed24d0
wnorowsky <wnorowsky.nicolas@gmail.com>
2024-11-03 14:19:07 -0300

    Uncomment put player route

--- back-end/src/routes/player.js
+++ back-end/src/routes/player.js
@@ -2,13 +2,11 @@ const express = require("express");
 const router = express.Router();
 const { playerController } = require("../controllers");
 
-// llamada a getAllPlayers: GET /player?page=1&limit=10
+// llamada a getAllPlayers: GET /player?page=1&limit=9
 router.get("/", playerController.getAllPlayers);
 router.post("/", playerController.createPlayer);
 router.get("/:id", playerController.getPlayerById);
 router.delete("/:id", playerController.deletePlayer);
-/* 
 router.put("/:id", playerController.updatePlayer);
-*/
-module.exports = router;
 
+module.exports = router;
