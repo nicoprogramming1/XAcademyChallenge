e7bcbb14367334a3b5b8f0114d05da358eda0145
wnorowsky <wnorowsky.nicolas@gmail.com>
2024-11-05 15:34:50 -0300

    Add User class

---
+++ back-end/src/models/User.js
@@ -0,0 +1,49 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+
+class User extends Model {}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      field: 'first_name'
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      field: 'last_name'
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.ENUM("Administrador", "Invitado"),
+      allowNull: false,
+      defaultValue: "Invitado",
+    },
+  },
+  {
+    sequelize,
+    modelName: "User",
+    tableName: "users",
+    timestamps: false,
+  }
+);
+
+module.exports = User;
