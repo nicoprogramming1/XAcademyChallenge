8d50a311353f136adfb4fb1a475b24b39317b85d
wnorowsky <wnorowsky.nicolas@gmail.com>
2024-11-05 19:11:16 -0300

    Add successMessage to user-registration comp and html

--- front-end/ownFIFA/src/app/views/users/user-registration/user-registration.component.ts
+++ front-end/ownFIFA/src/app/views/users/user-registration/user-registration.component.ts
@@ -1,6 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { UserService } from '../../../services/user.service';
 import { Role, User } from '../../../interfaces/user.interface';
 
@@ -9,38 +15,54 @@ import { Role, User } from '../../../interfaces/user.interface';
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './user-registration.component.html',
-  styleUrl: './user-registration.component.scss'
+  styleUrl: './user-registration.component.scss',
 })
 export class UserRegistrationComponent {
-  private userService = inject(UserService)
-  private fb = inject(FormBuilder)
+  private userService = inject(UserService);
+  private fb = inject(FormBuilder);
 
   public invalidForm: boolean = false;
-  public registerForm!: FormGroup
+  public registerForm!: FormGroup;
+  public successMessage: string | null = null;
 
   ngOnInit(): void {
     this.registerForm = this.fb.group({
-      firstName: new FormControl("", Validators.required),
-      lastName: new FormControl("", Validators.required),
-      email: new FormControl("", [Validators.required, Validators.email]),
-      password: new FormControl("", Validators.required)
-    })
+      firstName: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required),
+    });
   }
 
   onSubmit() {
-    if(this.registerForm.valid){
-      const formValues = this.registerForm.value
+    if (this.registerForm.valid) {
+      const formValues = this.registerForm.value;
 
       const user: User = {
         firstName: formValues.firstName,
         lastName: formValues.lastName,
         email: formValues.email,
         password: formValues.password,
-        role: Role.INVITADO
-      }
+        role: Role.INVITADO,
+      };
 
-      this.userService.saveUser()
-
+      this.userService.saveUser(user).subscribe({
+        next: (res) => {
+          console.log('Se ha registrado con éxito al usuario: ', res?.lastName);
+          this.invalidForm = false;
+          this.successMessage = 'El usuario se ha creado con éxito';
+        },
+        error: (err) => {
+          console.error('Ha habido un error en el registro', err);
+          this.invalidForm = true;
+        },
+        complete: () => {
+          this.resetForm();
+        },
+      });
+    } else {
+      this.invalidForm = true;
+      console.log('El formulario no está validado');
     }
   }
 
