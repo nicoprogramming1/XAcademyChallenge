562d12d4d5d05e6c8446f7be2234c323f216bcc5
wnorowsky <wnorowsky.nicolas@gmail.com>
2024-11-05 20:21:32 -0300

    Fix auth login

--- back-end/src/providers/authProvider.js
+++ back-end/src/providers/authProvider.js
@@ -6,17 +6,17 @@ const secretKey = process.env.JWT_SECRET || 'yourSecretKey';
 
 const authenticateUser = async (email, password) => {
   const user = await User.findOne({ where: { email } });
-  console.log("User found:", user);
+  console.log("Usuario encontrado:", user);
 
   if (!user) {
-    throw new Error('User not found');
+    throw new Error('Usuario no encontrado');
   }
 
   const passwordValid = await bcrypt.compare(password, user.password);
-  console.log("Password valid:", passwordValid);
+  console.log("Contraseña valida:", passwordValid);
 
   if (!passwordValid) {
-    throw new Error('Invalid credentials');
+    throw new Error('Credenciales invalidos');
   }
 
   const token = jwt.sign({ id: user.id, role: user.role }, secretKey, { expiresIn: '1h' });
