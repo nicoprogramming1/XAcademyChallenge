0ec58e1a701873c1beec9245ff33a87f101484bb
wnorowsky <wnorowsky.nicolas@gmail.com>
2024-11-05 15:43:06 -0300

    Create user model, controller and service

---
+++ back-end/src/routes/login.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const { userController } = require("../controllers");
+
+/* router.get("/", playerController.getAllPlayers);
+router.put("/:id", playerController.updatePlayer); */
+
+module.exports = router;
