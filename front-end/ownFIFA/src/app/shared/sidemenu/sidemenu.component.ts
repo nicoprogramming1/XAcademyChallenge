fe83100c88458acaaf92c5105208cdb5ee76ba47
wnorowsky <wnorowsky.nicolas@gmail.com>
2024-11-05 15:12:49 -0300

    Add routes to menu w/ menu service

--- front-end/ownFIFA/src/app/shared/sidemenu/sidemenu.component.ts
+++ front-end/ownFIFA/src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MenuService } from '../../services/menu.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidemenu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './sidemenu.component.html',
   styleUrl: './sidemenu.component.scss'
 })
 export class SidemenuComponent {
+  private menuService = inject(MenuService)
 
+  public menuItems = this.menuService.routes
 }
