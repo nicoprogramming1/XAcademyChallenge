e49fabcf24706c9ae765746b63c6c9be51d434c6
wnorowsky <wnorowsky.nicolas@gmail.com>
2024-11-08 16:30:43 -0300

    Add csv service

---
+++ front-end/ownFIFA/src/app/services/csv.service.ts
@@ -0,0 +1,34 @@
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, Observable, of } from 'rxjs';
+import { CsvUploadResponse } from '../interfaces/responses.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CsvService {
+  private apiUrl = environment.apiUrl;
+  private http = inject(HttpClient);
+
+  uploadCsv(file: File): Observable<CsvUploadResponse | null> {
+    const formData = new FormData();
+    formData.append('csvFile', file);
+
+    return this.http.post<CsvUploadResponse>(this.apiUrl, formData).pipe(
+      map((res) => {
+        if (res.success) {
+          console.log('Archivo CSV subido exitosamente:', res.message);
+          return res;
+        } else {
+          throw new Error(res.message);
+        }
+      }),
+      catchError((err) => {
+        const errorMessage = err.message || 'Error al subir el archivo CSV';
+        console.error(errorMessage);
+        return of(null);
+      })
+    );
+  }
+}
