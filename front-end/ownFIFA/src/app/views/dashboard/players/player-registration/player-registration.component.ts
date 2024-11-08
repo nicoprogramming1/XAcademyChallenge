3a3d470f5ca18ad697680598e7cdfcf727eb23c4
wnorowsky <wnorowsky.nicolas@gmail.com>
2024-11-08 12:57:18 -0300

    Add validation functions to each html register form control

--- front-end/ownFIFA/src/app/views/dashboard/players/player-registration/player-registration.component.ts
+++ front-end/ownFIFA/src/app/views/dashboard/players/player-registration/player-registration.component.ts
@@ -23,10 +23,7 @@ import { PlayerStateService } from '../../../../services/player-state.service';
 @Component({
   selector: 'app-player-registration',
   standalone: true,
-  imports: [
-    CommonModule,
-    ReactiveFormsModule
-  ],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './player-registration.component.html',
   styleUrl: './player-registration.component.scss',
 })
@@ -51,7 +48,10 @@ export class PlayerRegistrationComponent {
 
   ngOnInit(): void {
     this.registerForm = this.fb.group({
-      longName: new FormControl('', Validators.required),
+      longName: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
       age: new FormControl('', [Validators.required, Validators.min(1)]),
       playerPositions: this.fb.array([], Validators.required),
       playerFaceUrl: new FormControl('', Validators.required),
@@ -59,15 +59,15 @@ export class PlayerRegistrationComponent {
       nationalityName: new FormControl(''),
       preferredFoot: new FormControl(''),
       bodyType: new FormControl(''),
-      heightCm: new FormControl(''),
-      weightKg: new FormControl(''),
-      potential: new FormControl('', Validators.required),
-      overall: new FormControl('', Validators.required),
-      fifaVersion: new FormControl('', Validators.required),
-      fifaUpdate: new FormControl('', Validators.required),
-      passing: new FormControl(''),
-      dribbling: new FormControl(''),
-      shooting: new FormControl(''),
+      heightCm: new FormControl('', Validators.min(1)),
+      weightKg: new FormControl('', Validators.min(1)),
+      potential: new FormControl('', [Validators.required, Validators.min(1)]),
+      overall: new FormControl('', [Validators.required, Validators.min(1)]),
+      fifaVersion: new FormControl('', [Validators.required, Validators.min(1)]),
+      fifaUpdate: new FormControl('', [Validators.required, Validators.min(1)]),
+      passing: new FormControl('', Validators.min(1)),
+      dribbling: new FormControl('', Validators.min(1)),
+      shooting: new FormControl('', Validators.min(1)),
     });
   }
 
@@ -94,7 +94,7 @@ export class PlayerRegistrationComponent {
       const player: Player = {
         longName: formValues.longName,
         age: formValues.age,
-        playerPositions: formValues.playerPositions.join(','),  // convierte strings a array
+        playerPositions: formValues.playerPositions.join(','), // convierte strings a array
         playerFaceUrl: formValues.playerFaceUrl || null,
         clubName: formValues.clubName || null,
         nationalityName: formValues.nationalityName || null,
@@ -134,5 +134,199 @@ export class PlayerRegistrationComponent {
     this.registerForm.reset();
     (this.registerForm.get('playerPositions') as FormArray).clear(); // Limpia el array
     this.invalidForm = false;
+  }
+
+  showAgeErrors() {
+    const age = this.registerForm.get('age');
+
+    if (age?.touched && !age.valid) {
+      switch (true) {
+        case age.errors!['required']:
+          return 'La edad es obligatoria';
+        case age.errors!['min']:
+          return 'La edad debe ser un número positivo';
+        default:
+          return null;
+      }
+    }
+    return null;
+  }
+
+  showNameErrors() {
+    const longName = this.registerForm.get('longName');
+
+    if (longName?.touched && !longName.valid) {
+      switch (true) {
+        case longName.errors!['required']:
+          return 'El nombre es obligatoria';
+        case longName.errors!['minLength']:
+          return 'El nombre debe tener al menos 3 caracteres';
+        default:
+          return null;
+      }
+    }
+    return null;
+  }
+
+  showPlayerPositionsErrors() {
+    const playerPositions = this.registerForm.get('playerPositions');
+
+    if (playerPositions?.touched && !playerPositions.valid) {
+      switch (true) {
+        case playerPositions.errors!['required']:
+          return 'Debe seleccionar una posición';
+        default:
+          return null;
+      }
+    }
+    return null;
+  }
+
+  showPlayerFaceUrlErrors() {
+    const playerFaceUrl = this.registerForm.get('playerFaceUrl');
+
+    if (playerFaceUrl?.touched && !playerFaceUrl.valid) {
+      switch (true) {
+        case playerFaceUrl.errors!['required']:
+          return 'Debe seleccionar un rostro para el jugador';
+        default:
+          return null;
+      }
+    }
+    return null;
+  }
+
+  showHeightCmErrors() {
+    const heightCm = this.registerForm.get('heightCm');
+
+    if (heightCm?.touched && !heightCm.valid) {
+      switch (true) {
+        case heightCm.errors!['min']:
+          return 'La altura debe ser un número positivo';
+        default:
+          return null;
+      }
+    }
+    return null;
+  }
+
+  showWeightKgErrors() {
+    const weightKg = this.registerForm.get('weightKg');
+
+    if (weightKg?.touched && !weightKg.valid) {
+      switch (true) {
+        case weightKg.errors!['min']:
+          return 'El peso debe ser un número positivo';
+        default:
+          return null;
+      }
+    }
+    return null;
+  }
+
+  showPotentialErrors() {
+    const potential = this.registerForm.get('potential');
+
+    if (potential?.touched && !potential.valid) {
+      switch (true) {
+        case potential.errors!['required']:
+          return 'El potencial es obligatorio';
+        case potential.errors!['min']:
+          return 'El potencial debe ser un número positivo';
+        default:
+          return null;
+      }
+    }
+    return null;
+  }
+
+  showOverallErrors() {
+    const overall = this.registerForm.get('overall');
+
+    if (overall?.touched && !overall.valid) {
+      switch (true) {
+        case overall.errors!['required']:
+          return 'El nivel general es obligatorio';
+        case overall.errors!['min']:
+          return 'El nivel general debe ser un número positivo';
+        default:
+          return null;
+      }
+    }
+    return null;
+  }
+
+  showFifaVersionErrors() {
+    const fifaVersion = this.registerForm.get('fifaVersion');
+
+    if (fifaVersion?.touched && !fifaVersion.valid) {
+      switch (true) {
+        case fifaVersion.errors!['required']:
+          return 'Debes seleccionar una versión';
+        case fifaVersion.errors!['min']:
+          return 'La versión debe ser un número positivo';
+        default:
+          return null;
+      }
+    }
+    return null;
+  }
+
+  showFifaUpdateErrors() {
+    const fifaUpdate = this.registerForm.get('fifaUpdate');
+
+    if (fifaUpdate?.touched && !fifaUpdate.valid) {
+      switch (true) {
+        case fifaUpdate.errors!['required']:
+          return 'Debes seleccionar un update';
+        case fifaUpdate.errors!['min']:
+          return 'El update debe ser un número positivo';
+        default:
+          return null;
+      }
+    }
+    return null;
+  }
+
+  showPassingErrors() {
+    const passing = this.registerForm.get('passing');
+
+    if (passing?.touched && !passing.valid) {
+      switch (true) {
+        case passing.errors!['min']:
+          return 'El pase debe ser un número positivo';
+        default:
+          return null;
+      }
+    }
+    return null;
+  }
+
+  showDribblingErrors() {
+    const dribbling = this.registerForm.get('dribbling');
+
+    if (dribbling?.touched && !dribbling.valid) {
+      switch (true) {
+        case dribbling.errors!['min']:
+          return 'El regateo debe ser un número positivo';
+        default:
+          return null;
+      }
+    }
+    return null;
+  }
+
+  showShootingErrors() {
+    const shooting = this.registerForm.get('shooting');
+
+    if (shooting?.touched && !shooting.valid) {
+      switch (true) {
+        case shooting.errors!['min']:
+          return 'El disparo debe ser un número positivo';
+        default:
+          return null;
+      }
+    }
+    return null;
   }
 }
