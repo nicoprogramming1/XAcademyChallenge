18f7ade05be48aa77ef94f3feff54b88fc6c1e23
wnorowsky <wnorowsky.nicolas@gmail.com>
2024-11-09 12:06:47 -0300

    Add csv export controller

--- back-end/src/controllers/csvDataController.js
+++ back-end/src/controllers/csvDataController.js
@@ -1,8 +1,9 @@
 const csvDataService = require("../services/csvDataService");
+const fs = require("fs")
 
 exports.uploadCSV = async (req, res) => {
-  const file = req.file
-  console.log("Desde el controller csv, file: ", file)
+  const file = req.file;
+  console.log("Desde el controller csv, file: ", file);
   try {
     if (!file) {
       // verificar que el archivo fue subido correctamente
@@ -12,7 +13,7 @@ exports.uploadCSV = async (req, res) => {
       });
     }
 
-    await csvDataService.processCSV(file.path);
+    await csvDataService.importCSV(file.path);
     res.status(200).json({
       success: true,
       message: "Datos del CSV importados exitosamente",
@@ -21,7 +22,41 @@ exports.uploadCSV = async (req, res) => {
     console.error("Error al importar CSV:", error);
     res.status(500).json({
       succes: false,
-      message: `Error al importar CSV: ${error}`
+      message: `Error al importar CSV: ${error}`,
     });
+  }
+};
+
+exports.downloadCSV = async (req, res) => {
+  try {
+    const csvFilePath = await csvDataService.exportCSV(); // Genera el archivo CSV
+
+    if (!csvFilePath || !fs.existsSync(csvFilePath)) {
+      return res.status(400).send('Error al exportar el CSV: El archivo no existe.');
+    }
+
+    // Creamos un flujo de lectura (stream) del archivo
+    const fileStream = fs.createReadStream(csvFilePath);
+
+    // Configuramos los encabezados de la respuesta para la descarga del archivo
+    res.setHeader('Content-Type', 'text/csv');
+    res.setHeader('Content-Disposition', `attachment; filename=database_export_${Date.now()}.csv`);
+
+    // Pipe el archivo al response para enviar el archivo al cliente
+    fileStream.pipe(res);
+
+    // Eliminar el archivo después de enviarlo (esto es opcional)
+    fileStream.on('end', () => {
+      fs.unlinkSync(csvFilePath); // Eliminamos el archivo del servidor
+    });
+
+    fileStream.on('error', (err) => {
+      console.error('Error al enviar el archivo:', err);
+      res.status(500).send('Error al enviar el archivo.');
+    });
+
+  } catch (error) {
+    console.error("Ha ocurrido un error en el controller csv:", error);
+    res.status(500).send('Ha ocurrido un error al exportar el archivo CSV.');
   }
 };
