85db0af4bf9e6769fbd90f8b8faa513881d77e05
wnorowsky <wnorowsky.nicolas@gmail.com>
2024-11-11 13:23:07 -0300

    Fix routes and disposition

--- front-end/ownFIFA/src/app/app.component.ts
+++ front-end/ownFIFA/src/app/app.component.ts
@@ -1,16 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
-import { LogoutComponent } from './views/users/login/logout/logout.component';
 import { SidemenuComponent } from './shared/sidemenu/sidemenu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, LogoutComponent, SidemenuComponent],
+  imports: [RouterOutlet, HeaderComponent, SidemenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'ownFIFA';
+  private router = inject(Router)
+  public loginPage: boolean = false
+
+  isLoginPage(): boolean {
+    this.loginPage = this.router.url === '/login';
+    return this.loginPage
+  }
 }
