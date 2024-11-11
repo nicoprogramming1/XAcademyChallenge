a300f9d96c4ec4fe912b0306715fa777d9058ee2
wnorowsky <wnorowsky.nicolas@gmail.com>
2024-11-10 21:22:16 -0300

    Protect endpoint with roleMdw

--- back-end/src/routes/user.js
+++ back-end/src/routes/user.js
@@ -1,7 +1,9 @@
 const userController = require("../controllers/userController");
 const express = require("express");
 const router = express.Router();
+const Roles = require("../models")
+const roleMdw = require("../middleware")
 
-router.post("/", userController.createUser);
+router.post("/", roleMdw(Roles.ADMIN), userController.createUser);
 
 module.exports = router;
