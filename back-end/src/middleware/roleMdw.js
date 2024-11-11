0a74f02c4f082408e109437415835b5070cc5b6f
wnorowsky <wnorowsky.nicolas@gmail.com>
2024-11-11 19:52:19 -0300

    Fix roleMdw, working!

--- back-end/src/middleware/roleMdw.js
+++ back-end/src/middleware/roleMdw.js
@@ -1,6 +1,8 @@
 const roleMdw = (requiredRole) => {
   return (req, res, next) => {
     const user = req.user; // Usuario autenticado por passport
+    console.log("Desde el roleMdw - User:", user);
+    console.log("Desde el roleMdw - Required Role:", requiredRole);
 
     if (user && user.role === requiredRole) {
       return next(); // si todo ok sigue
--- back-end/src/routes/user.js
+++ back-end/src/routes/user.js
@@ -1,8 +1,8 @@
 const userController = require("../controllers/userController");
 const express = require("express");
 const router = express.Router();
-const Roles = require("../models")
-const roleMdw = require("../middleware/roleMdw")
+const { Roles } = require("../models");
+const roleMdw = require("../middleware/roleMdw");
 
 router.post("/", roleMdw(Roles.ADMIN), userController.createUser);
 
